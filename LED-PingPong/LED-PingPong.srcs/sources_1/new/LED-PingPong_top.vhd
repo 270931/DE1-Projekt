5c2ec87797b1ab90e131d77e2f0ab3fb47042e90
WhyMe <270931@vutbr.cz>
2026-03-28 10:20:52 +0100

    Declaration of ports for GameLogic + top
    Plus I've corrected the wrong size of 'seg' in game logic and the top file.

--- LED-PingPong/LED-PingPong.srcs/sources_1/new/LED-PingPong_top.vhd
+++ LED-PingPong/LED-PingPong.srcs/sources_1/new/LED-PingPong_top.vhd
@@ -7,16 +7,27 @@ use IEEE.STD_LOGIC_1164.ALL;
 ----------------------------------------------------------------
 entity LED_PingPong_top is
 --  TODO: might need to be expanded
-    Port ( LED_play : out STD_LOGIC_VECTOR (15 downto 0);
-           dp : out STD_LOGIC;
-           seg : out STD_LOGIC_VECTOR (15 downto 0);
-           clk : in STD_LOGIC;
-           rst : in STD_LOGIC);
+    Port ( 
+           -- IN facing ports
+           clk      : in STD_LOGIC;
+           btnc     : in STD_LOGIC;
+           btnl     : in STD_LOGIC;
+           btnr     : in STD_LOGIC;
+           
+           -- OUT facing ports
+           seg      : out STD_LOGIC_VECTOR (6 downto 0);
+           led      : out STD_LOGIC_VECTOR (15 downto 0);
+           led16_b  : out STD_LOGIC;
+           anode    : out STD_LOGIC_VECTOR (7 downto 0);
+           dp       : out STD_LOGIC
+           );
 end LED_PingPong_top;
 
 architecture Behavioral of LED_PingPong_top is
 
 begin
+    
+    -- TODO: connect the instances according to schematic
 
 
 end Behavioral;
