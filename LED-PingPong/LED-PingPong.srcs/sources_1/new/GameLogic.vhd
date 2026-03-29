800f189ac5125d83b77b6570ca0a04801ea11957
WhyMe <270931@vutbr.cz>
2026-03-29 11:36:52 +0200

    Started on the main Game cycle + added letter to bin2seg

--- LED-PingPong/LED-PingPong.srcs/sources_1/new/GameLogic.vhd
+++ LED-PingPong/LED-PingPong.srcs/sources_1/new/GameLogic.vhd
@@ -18,11 +18,74 @@ entity GameLogic is
            dp       : out STD_LOGIC);
 end GameLogic;
 
-architecture Behavioral of GameLogic is
+architecture Behavioral of GameLogic is 
+
+    signal signal_seg  : STD_LOGIC_VECTOR(6 downto 0);
+    signal signal_anode: STD_LOGIC_VECTOR(7 downto 0);
+
+    -- Definition of precedure 'MaximumScore'
+    procedure MaximumScore(
+        variable player_L_score     : in integer;
+        variable player_R_score     : in integer;
+        variable   endOfGame          : out STD_LOGIC
+        ) is
+        
+        begin
+            if(player_L_score > 3) then
+                endOfGame := '1';
+                return;
+            end if;
+            
+            if(player_R_score > 3) then
+                endOfGame := '1';
+                return;
+            end if;
+        end procedure MaximumScore;            
 
 begin
+    
+    
+    process(clk) is
+       
+       variable player_L_score  : integer;
+       variable player_R_score  : integer;
+       
+       variable ball_position   : STD_LOGIC_VECTOR(15 downto 0);
+       variable game_start      : STD_LOGIC;
+       
+    begin
+        
+        if rising_edge(clk) then
+            
+            -- check if reset signal has been recieved
+            if(rst = '1') then
+                player_L_score  := 0;
+                player_R_score  := 0;
+                ball_position   := b"0000_0001_1000_0000";
+            end if;
+            
+            
+            -- accual game body
+            if(game_start = '1') then
+                -- game
+                
+                -- update the ball position
+                -- will need slowing down 
+                led <= ball_position;
+            
+            
+            -- for starting the game
+            else
+                -- TODO:
+                -- Display on 7-segment 'PLAY'
+                
+                if( ( (player_L = '1') OR (player_R = '1') ) AND (game_start /= '1') ) then
+                    game_start := '1';
+                end if;   
+                
+            end if; 
+        
 
-    --TODO: how will this work??
-
-
+        end if;
+    end process;
 end Behavioral;
--- LED-PingPong/LED-PingPong.srcs/sources_1/new/bin2seg.vhd
+++ LED-PingPong/LED-PingPong.srcs/sources_1/new/bin2seg.vhd
@@ -3,7 +3,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity bin2seg is
-    Port ( bin : in STD_LOGIC_VECTOR (3 downto 0);
+    Port ( bin : in STD_LOGIC_VECTOR (4 downto 0);
            seg : out STD_LOGIC_VECTOR (7 downto 0));
 end bin2seg;
 
@@ -14,26 +14,51 @@ begin
     p_7seg_decoder : process (bin) is
     begin
         case bin is
+        
+            -- b"0_0000"
             when x"0" =>
                 seg <= b"000_0001";
+            -- b"0_0001"
             when x"1" =>
                 seg <= b"100_1111";
+            -- b"0_0010"
             when x"2" =>
                 seg <= b"001_0010";
+            -- b"0_0011"
             when x"3" =>
                 seg <= b"000_0110";
+            -- b"0_0100"
             when x"4" =>
                 seg <= b"100_1100";
+            -- b"0_0101"
             when x"5" =>
                 seg <= b"010_0100";
+            -- b"0_0110"
             when x"6" =>
                 seg <= b"010_0000";
+            -- b"0_0111"
             when x"7" =>
                 seg <= b"000_1111";
+            -- b"0_1000"
             when x"8" =>
                 seg <= b"000_0000";
+            -- b"0_1001"
             when x"9" =>
                 seg <= b"000_0100";
+            
+            -- Displaying text
+            -- "P"
+            when b"1_0001" =>
+                seg <= b"001_1000";
+            -- "L"
+            when b"1_0001" =>
+                seg <= b"111_0001";
+            -- "A"
+            when b"1_0001" =>
+                seg <= b"000_1000";
+            -- "Y"
+            when b"1_0001" =>
+                seg <= b"101_1000";
                 
             when others =>
                 seg <= b"111_1111";  -- All segments off
