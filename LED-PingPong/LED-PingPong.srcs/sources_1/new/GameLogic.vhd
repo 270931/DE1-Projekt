96b34a2763fc1e8d86294d5788e1ef2fe0bfb66d
WhyMe <270931@vutbr.cz>
2026-04-02 11:08:46 +0200

    Conversion to FSM design
    Předělal jsem komponentu "GameLogic" na stavový automat. Zároveň jsem ke každému stavu napsal, co má za úkol a kam se přejde při splnění určité podmínky.
    Dále jsem podle schématu z GitHubu spojil v Top Level file instance debouce a GameLogic. Pravděpodobně bude potřeba přidat nějaký ten clk_en, hlavně teda pro rychlost míčku, protože teď by celé hrací pole přeletěl za 160 ns (což je fakt rychle).
    Ve schéma jsem pouze opravil pár překlepů a nastavil nepoužívané výstupy  'debouceX' na 'open'.

--- LED-PingPong/LED-PingPong.srcs/sources_1/new/GameLogic.vhd
+++ LED-PingPong/LED-PingPong.srcs/sources_1/new/GameLogic.vhd
@@ -20,72 +20,84 @@ end GameLogic;
 
 architecture Behavioral of GameLogic is 
 
-    signal signal_seg  : STD_LOGIC_VECTOR(6 downto 0);
-    signal signal_anode: STD_LOGIC_VECTOR(7 downto 0);
+    -- to change the winnig score, modify this value
+    constant WIN_SCORE : integer := 10;
 
-    -- Definition of precedure 'MaximumScore'
-    procedure MaximumScore(
-        variable player_L_score     : in integer;
-        variable player_R_score     : in integer;
-        variable   endOfGame          : out STD_LOGIC
-        ) is
-        
-        begin
-            if(player_L_score > 3) then
-                endOfGame := '1';
-                return;
-            end if;
-            
-            if(player_R_score > 3) then
-                endOfGame := '1';
-                return;
-            end if;
-        end procedure MaximumScore;            
+    -- signals needed to run the game
+    signal signal_seg     : STD_LOGIC_VECTOR(6 downto 0);
+    signal signal_anode   : STD_LOGIC_VECTOR(7 downto 0);
+    signal player_R_score : integer range 0 to WIN_SCORE;
+    signal player_L_score : integer range 0 to WIN_SCORE;
+    signal ball_position  : STD_LOGIC_VECTOR(15 downto 0);
+    
+    -- possible states of the game
+    type state_type is (IDLE, START, PLAYING, END_OF_ROUND, END_OF_GAME);
+    signal game_state : state_type;
 
 begin
     
-    
-    process(clk) is
-       
-       variable player_L_score  : integer;
-       variable player_R_score  : integer;
-       
-       variable ball_position   : STD_LOGIC_VECTOR(15 downto 0);
-       variable game_start      : STD_LOGIC;
-       
+    game: process(clk)
     begin
         
         if rising_edge(clk) then
             
             -- check if reset signal has been recieved
             if(rst = '1') then
-                player_L_score  := 0;
-                player_R_score  := 0;
-                ball_position   := b"0000_0001_1000_0000";
-            end if;
-            
-            
-            -- accual game body
-            if(game_start = '1') then
-                -- game
-                
-                -- update the ball position
-                -- will need slowing down 
-                led <= ball_position;
-            
-            
-            -- for starting the game
+                player_L_score  <= 0;
+                player_R_score  <= 0;
+                ball_position   <= b"0000_0001_1000_0000";
+                game_state      <= IDLE;
+                signal_seg      <= b"111_1111";
             else
-                -- TODO:
-                -- Display on 7-segment 'PLAY'
                 
-                if( ( (player_L = '1') OR (player_R = '1') ) AND (game_start /= '1') ) then
-                    game_start := '1';
-                end if;   
-                
-            end if; 
+                case game_state is
+                    
+                    when IDLE =>
+                    -- In this state, the game is waiting for input from any player to transition to START state
+                    
+                        -- If input is detected from any or all players, the game will transition with the next
+                        --  clock cycle to 'START' state
+                        if(player_L = '1' OR player_R = '1') then
+                            game_state <= START;
+                        else
+                            -- Display the score on 7-segment in the format of X00--00X (where X is off, and left 00
+                            --  is player L score and vice versa)
+                        end if;
+                        
+                    when START =>
+                    -- In this state, decide on the direction of the ball movement (somehow), and also display the
+                    --  word 'PLAY' on the the centre 4 7-segments.
+                    -- After that, advace to 'PLAYING' state
+                    
+                    when PLAYING =>
+                    -- In this state, advance the ball according to slowed clock signal in the decided direction
+                    -- When ball is in the player teritory (for the left player LED15 is on, for right LED0 is on)
+                    --  check for the 'player_X' signal within time of one ball advancement.
+                    -- If succesful, reverse the direction of ball, flash 'led16_b' and repeate.
+                    -- If failed, advance to the 'END_OF_ROUND' state. 
+                    
+                    when END_OF_ROUND =>
+                    -- In this state, on the basis of the ball position, recalculate the coresponding player score.
+                    -- If 'player_X' score if equal to 'WIN_SCORE', advace to the 'END_OF_GAME' state.
+                    -- If 'palyer_X' score is less than 'WIN_SCORE' advance to the 'IDLE' state.
+                    
+                    when END_OF_GAME =>
+                    -- In this state, flash repeatedly all playing LEDs and display either 'PLAYERX1' or 'PLAYERX2'
+                    --  on the 7-segment (X meaning segment is off).
+                    -- Remain in this state until manual reset.
+                    
+                    when others =>
+                    -- If something unexpected happens, go to idle state.
+                        game_state <= START;
+                end case;
+            
+            
+            -- reset 'if'
+            end if;
         
-
+        -- rising_edge 'if'
         end if;
+    
+    -- 'game' process
     end process;
 end Behavioral;
--- LED-PingPong/LED-PingPong.srcs/sources_1/new/LED-PingPong_top.vhd
+++ LED-PingPong/LED-PingPong.srcs/sources_1/new/LED-PingPong_top.vhd
@@ -9,25 +9,96 @@ entity LED_PingPong_top is
 --  TODO: might need to be expanded
     Port ( 
            -- IN facing ports
-           clk      : in STD_LOGIC;
-           btnc     : in STD_LOGIC;
-           btnl     : in STD_LOGIC;
-           btnr     : in STD_LOGIC;
+           clk      : in STD_LOGIC;     -- system clock signal (the 10 ns one)
+           btnc     : in STD_LOGIC;     -- system reset button (center button)
+           btnl     : in STD_LOGIC;     -- left player button
+           btnr     : in STD_LOGIC;     -- right player button
            
            -- OUT facing ports
-           seg      : out STD_LOGIC_VECTOR (6 downto 0);
-           led      : out STD_LOGIC_VECTOR (15 downto 0);
-           led16_b  : out STD_LOGIC;
-           anode    : out STD_LOGIC_VECTOR (7 downto 0);
-           dp       : out STD_LOGIC
+           seg      : out STD_LOGIC_VECTOR (6 downto 0);    -- current 7-segment display data
+           led      : out STD_LOGIC_VECTOR (15 downto 0);   -- player hir registered LED
+           led16_b  : out STD_LOGIC;                        -- playing field LEDs
+           anode    : out STD_LOGIC_VECTOR (7 downto 0);    -- current 7-segment active (active low)
+           dp       : out STD_LOGIC                         -- no decimal point
            );
 end LED_PingPong_top;
 
+
+
 architecture Behavioral of LED_PingPong_top is
+
+    --Internal signals for connecting components
+    signal sig_player_L_press : STD_LOGIC;
+    signal sig_player_R_press : STD_LOGIC;
+    
+    
+    -- component "Debouce" declaration
+    component debounce is
+        Port ( clk       : in STD_LOGIC;
+               rst       : in STD_LOGIC;
+               btn_in    : in STD_LOGIC;
+               btn_state : out STD_LOGIC;
+               btn_press : out STD_LOGIC
+             );
+    end component debounce;
+    
+    
+    -- component "GameLogic" declaration
+    component GameLogic is
+        Port ( 
+               -- IN facing ports
+               clk      : in STD_LOGIC;
+               rst      : in STD_LOGIC;
+               player_L : in STD_LOGIC;
+               player_R : in STD_LOGIC;
+               
+               -- OUT facing ports
+               seg      : out STD_LOGIC_VECTOR (6 downto 0);
+               led      : out STD_LOGIC_VECTOR (15 downto 0);
+               led16_b  : out STD_LOGIC;
+               anode    : out STD_LOGIC_VECTOR (7 downto 0);
+               dp       : out STD_LOGIC);
+    end component GameLogic;
+
 
 begin
     
-    -- TODO: connect the instances according to schematic
+    -- debounce for PlayerL input button
+    debounceL: debounce
+        port map(
+            clk       => clk,
+            rst       => btnc,
+            btn_in    => btnl,
+            btn_press => sig_player_L_press,
+            btn_state => open
+        );
+    
+    -- debounce for PlayerR input button
+    debounceR: debounce
+        port map(
+            clk       => clk,
+            rst       => btnc,
+            btn_in    => btnr,
+            btn_press => sig_player_R_press,
+            btn_state => open
+        );
+    
+    -- instance of main GameLogic component
+    game: GameLogic
+        port map(
+            clk => clk,
+            rst => btnc,
+            player_L => sig_player_L_press,
+            player_R => sig_player_R_press,
+            
+            seg => seg,
+            led => led,
+            led16_b => led16_b,
+            anode => anode,
+            dp => dp
+        );
+        
+     -- probbably need to add clk_en somewhere
 
 
 end Behavioral;
