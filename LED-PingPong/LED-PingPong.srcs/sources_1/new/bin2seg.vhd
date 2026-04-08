d78f257f098e814c36c5f0e1b3e65ac4b55aec47
Damian <160263257+zivyzit58@users.noreply.github.com>
2026-04-08 19:09:17 +0200

    Update bin2seg.vhd
    better arrangement

--- LED-PingPong/LED-PingPong.srcs/sources_1/new/bin2seg.vhd
+++ LED-PingPong/LED-PingPong.srcs/sources_1/new/bin2seg.vhd
@@ -16,52 +16,37 @@ begin
         case bin is
         
             -- b"0_0000"
-            when x"0" =>
-                seg <= b"000_0001";
+            when x"0" => seg <= b"000_0001";
             -- b"0_0001"
-            when x"1" =>
-                seg <= b"100_1111";
+            when x"1" => seg <= b"100_1111";
             -- b"0_0010"
-            when x"2" =>
-                seg <= b"001_0010";
+            when x"2" => seg <= b"001_0010";
             -- b"0_0011"
-            when x"3" =>
-                seg <= b"000_0110";
+            when x"3" => seg <= b"000_0110";
             -- b"0_0100"
-            when x"4" =>
-                seg <= b"100_1100";
+            when x"4" => seg <= b"100_1100";
             -- b"0_0101"
-            when x"5" =>
-                seg <= b"010_0100";
+            when x"5" => seg <= b"010_0100";
             -- b"0_0110"
-            when x"6" =>
-                seg <= b"010_0000";
+            when x"6" => seg <= b"010_0000";
             -- b"0_0111"
-            when x"7" =>
-                seg <= b"000_1111";
+            when x"7" => seg <= b"000_1111";
             -- b"0_1000"
-            when x"8" =>
-                seg <= b"000_0000";
+            when x"8" => seg <= b"000_0000";
             -- b"0_1001"
-            when x"9" =>
-                seg <= b"000_0100";
+            when x"9" => seg <= b"000_0100";
             
             -- Displaying text
             -- "P"
-            when b"1_0001" =>
-                seg <= b"001_1000";
+            when b"1_0001" => seg <= b"001_1000";
             -- "L"
-            when b"1_0001" =>
-                seg <= b"111_0001";
+            when b"1_0001" => seg <= b"111_0001";
             -- "A"
-            when b"1_0001" =>
-                seg <= b"000_1000";
+            when b"1_0001" => seg <= b"000_1000";
             -- "Y"
-            when b"1_0001" =>
-                seg <= b"101_1000";
+            when b"1_0001" => seg <= b"101_1000";
                 
-            when others =>
-                seg <= b"111_1111";  -- All segments off
+            when others => seg <= b"111_1111";  -- All segments off
         end case;
     end process p_7seg_decoder;
 
