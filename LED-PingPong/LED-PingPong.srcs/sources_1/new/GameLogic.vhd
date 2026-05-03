d094cf6103d2f046a7eac676f9e39d6cc01b8521
WhyMe <270931@vutbr.cz>
2026-05-03 11:54:47 +0200

    Implementace smeče
    Ale Timing Analýza nevychází

--- LED-PingPong/LED-PingPong.srcs/sources_1/new/GameLogic.vhd
+++ LED-PingPong/LED-PingPong.srcs/sources_1/new/GameLogic.vhd
@@ -50,6 +50,7 @@ architecture Behavioral of GameLogic is
 
     -- signals needed to run the game
     signal reset_request  : STD_LOGIC;
+    signal speed_rst      : STD_LOGIC;
     signal player_R_score : integer range 0 to WIN_SCORE;
     signal player_L_score : integer range 0 to WIN_SCORE;
     signal ball_position  : STD_LOGIC_VECTOR(15 downto 0);
@@ -60,10 +61,11 @@ architecture Behavioral of GameLogic is
     signal ball_direction : STD_LOGIC;
     signal ball_tick      : STD_LOGIC;
     
-    -- Signály pro dynamickou rychlost
+    -- Dynamic speed signals
     signal hit_event      : STD_LOGIC := '0';
-    signal speed_rst    : STD_LOGIC;
     signal dynamic_tick   : integer range 0 to 10_000_000;
+    signal boost_factor   : integer range 1 to 3 := 1;
+    signal effective_tick : integer range 0 to 10_000_000;
     
     -- possible states of the game's FST
     type state_type is (IDLE, START, PLAYING, END_OF_ROUND, END_OF_GAME);
@@ -82,12 +84,12 @@ begin
         );
     
     -- Clock for 1 ball movement in either direction
-    -- Teď používá dynamický limit místo konstanty tick_time
+    -- IMPORTANT: uses 'effective_tick', which is ratio between (dynamic_tick / boost_factor)  
     clock_ball : clk_en
         port map (
             clk       => clk,
             rst       => reset_request,
-            sig_limit => dynamic_tick,
+            sig_limit => effective_tick,
             ce        => ball_tick
         );
         
@@ -112,8 +114,12 @@ begin
     
     -- update the LED playing field according to the ball position
     led <= ball_position;
-    -- disable the decimal point
-    dp <= '1';
+    
+
+    -- Effective tick logic, lower tick = higher frequence = ball go brrr
+    effective_tick <= dynamic_tick      when boost_factor = 1 else
+                      dynamic_tick / 2  when boost_factor = 2 else
+                      dynamic_tick / 3;
     
     game: process(clk)
     begin
@@ -125,29 +131,26 @@ begin
                 player_L_score  <= 0;
                 player_R_score  <= 0;
                 ball_position   <= b"0000_0001_1000_0000";
-                displayed_text  <= (others => '1');         -- IMPORTANT!! active low (svítí '0')
+                displayed_text  <= (others => '1');         -- IMPORTANT!! active low (sviti '0')
                 led16_b         <= '0';
                 reset_request   <= '1';
-                speed_rst       <= '1';
+                speed_rst       <= '1';                     -- reset speed_controller
                 hit_event       <= '0';
+                boost_factor    <= 1;                       -- DEFAULT ball speed
                 game_state      <= IDLE;
             else
                 
+                -- DEFAULT
                 hit_event <= '0';
-                speed_rst <='0';
+                speed_rst <= '0';
                 
                 case game_state is
                     
                     when IDLE =>
                     -- In this state, the game is waiting for input from any player to transition to START state
                     
-                        -- If input is detected from any or all players, the game will transition with the next
-                        --  clock cycle to 'START' state                                        DONE
                         if(player_L = '1' OR player_R = '1') then
-                        
-                            -- reset_request for counter in the next game state                 DONE
                             reset_request <= '1';
-                            -- advance gamestate
                             game_state <= START;
                         else
                             --                   _         L                    player L score                          _         -         P               player R score                               _
@@ -157,29 +160,22 @@ begin
                         
                         
                     when START =>
-                    -- In this state, decide on the direction of the ball movement (somehow)    DONE
-                    -- Display the word 'PLAY' on the the center 4 7-segments.                  DONE
-                    -- After that, advace to 'PLAYING' state                                    DONE
-                    -- Kdyz hra zacne, spusti se TIMER, ten generuje TICK o nejake f, counter bude pocitat uspesne odpaly, po X odpalech se tick zrychly a zvetsi obtiznost hry.
+                    -- Display the word 'PLAY' on the the center 4 7-segments.
+                    -- After that, advace to 'PLAYING' state.
                         
-                        -- Start the clk_en
                         reset_request <= '0';
-                 
                         
                                          --   _     _     P     L     A     Y     _     _
                         displayed_text <= b"11111_11111_10001_10010_10011_10100_11111_11111";
                         
-                        -- Once 4 secs is up, decide on the direction and continue
                         if (display_text = '1') then
                         
-                        -- Decide on the ball direction
                             if(player_L_score >= player_R_score) then 
                                 ball_direction <= '1';
                             else 
                                 ball_direction <= '0';
                             end if;
                             
-                            -- Advance the next game state
                             game_state <= PLAYING;
                             
                         end if;
@@ -187,11 +183,12 @@ begin
                     
                     
                     when PLAYING =>
-                    -- In this state, advance the ball according to slowed clock signal in the decided direction
-                    -- When ball is in the player teritory (for the left player LED15 is on, for right LED0 is on)
-                    --  check for the 'player_X' signal within time of one ball advancement.
-                    -- If sucessful, reverse the direction of ball  and repeate.
-                    -- If failed, flash 'led16_b' and advance to the 'END_OF_ROUND' state.
+                    -- 3-LED hit zone on each side:
+                    --   L: bit 15 (outer) = 1x, bit 14 (middle) = 2x, bit 13 (inside) = 3x
+                    --   R: bit 0  (outer) = 1x, bit 1  (middle) = 2x, bit 2  (inside) = 3x
+                    --
+                    -- Boost only lasts on flight, until it enters the zone of opposite player 
+                    -- then it returnts to 1x
                     
                         -- Blank the 7-segment display
                         displayed_text <= (others => '1');
@@ -202,25 +199,47 @@ begin
                         -- Stop reseting the timer
                         reset_request <= '0';
                         
-                        -- INDEPENDENT DETECTION of ball hit
-                        -- If left player button is pressed and ball is in their teritory...
-                        if (player_L = '1' and ball_position(15) = '1') then
-                            -- SUCCESS:
-                            -- ... reverse the ball direction ...
+                        -- LEFT player hit detection 
+                        if (player_L = '1' and ball_direction = '1' and 
+                            (ball_position(15) = '1' or ball_position(14) = '1' or ball_position(13) = '1')) then
+                            -- Boost selection logic
+                            if ball_position(15) = '1' then
+                                boost_factor <= 1;   
+                            elsif ball_position(14) = '1' then
+                                boost_factor <= 2;   
+                            else
+                                boost_factor <= 3;   -- If player hit serve, then boost the ball speed 3x
+                            end if;
+                            -- bounce
                             ball_direction <= '0';
-                            -- ... update ball position ...
                             ball_position <= '0' & ball_position(15 downto 1);
-                            hit_event <= '1'; -- +1 hit to the speed
-                            -- reset the timer now
-                            reset_request <= '1';
-                            
-                        elsif (player_R = '1' and ball_position(0) = '1') then
-                            -- SUCCESS
+                            hit_event <= '1';           
+                            reset_request <= '1';        
+                        
+                        -- RIGHT player hit detection
+                        elsif (player_R = '1' and ball_direction = '0' and
+                               (ball_position(0) = '1' or ball_position(1) = '1' or ball_position(2) = '1')) then
+                            -- Boost selection logic
+                            if ball_position(0) = '1' then
+                                boost_factor <= 1;
+                            elsif ball_position(1) = '1' then
+                                boost_factor <= 2;
+                            else
+                                boost_factor <= 3;   -- If player hit serve, then boost the ball speed 3x 
+                            end if;
                             ball_direction <= '1';
                             ball_position <= ball_position(14 downto 0) & '0';
-                            hit_event <= '1'; -- +1 hit to the speed
-                            -- reset the timer now
+                            hit_event <= '1';
                             reset_request <= '1';
+                        
+                        else
+                            if ball_direction = '0' and 
+                               (ball_position(0) = '1' or ball_position(1) = '1' or ball_position(2) = '1') then
+                                boost_factor <= 1;
+                            elsif ball_direction = '1' and
+                                  (ball_position(15) = '1' or ball_position(14) = '1' or ball_position(13) = '1') then
+                                boost_factor <= 1;
+                            end if;
                         end if;
 
                         -- BALL MOVEMENT update and MISSED HIT
@@ -230,14 +249,10 @@ begin
                                 -- Ball movement <- LEFT
                                 if(ball_position(15) = '1') then
                                     -- MISSED
-                                    -- MISSED is when, ball is in player teritory AND timer has expired a.k.a.
-                                    --         ball would have gone past the playing field
                                     player_R_score <= player_R_score + 1;
-                                    -- Flash led16_b upon MISSED
                                     led16_b <= '1';
                                     game_state <= END_OF_ROUND;
                                 else
-                                    -- Ball is not in player teritory
                                     ball_position <= ball_position(14 downto 0) & '0';
                                 end if;
                                 
@@ -246,7 +261,6 @@ begin
                                 if(ball_position(0) = '1') then
                                     -- MISSED
                                     player_L_score <= player_L_score + 1;
-                                    -- Flash led16_b upon MISSED
                                     led16_b <= '1';
                                     game_state <= END_OF_ROUND;
                                 else
@@ -259,64 +273,45 @@ begin
                 
                     
                     when END_OF_ROUND =>
-                    -- In this state, reset the ball position in case of another round will take place.
-                    -- If 'player_X' score if equal to 'WIN_SCORE', advace to the 'END_OF_GAME' state.
-                    -- If 'palyer_X' score is less than 'WIN_SCORE' advance to the 'IDLE' state.
-                    
                     -- Reset the ball position
                     ball_position   <= b"0000_0001_1000_0000";
                     
-                    speed_rst <= '1';
+                    -- Boost reset between servs
+                    boost_factor    <= 1;
                     
-                    -- Check, if entire game should end
                     if(player_L_score = WIN_SCORE OR player_R_score = WIN_SCORE) then
                         game_state <= END_OF_GAME;
                     else
-                    -- If not, start next round
                         game_state <= IDLE;
                     end if;
                     
                     when END_OF_GAME =>
-                    -- In this state, flash repeatedly all playing LEDs and display either 'PLAYERX1' or 'PLAYERX2'
-                    --  on the 7-segment (X meaning segment is off).
-                    -- Remain in this state until manual reset.
+                    -- Flash all LEDs, display winner, wait for manual reset.
                     
-                    -- Playing LEDs flashing
                         if (ball_tick = '1') then
-                            -- ball_position(15) is only used as 'memory' of sort, to know what the previous state was
                             if (ball_position(15) = '1') then
-                            -- If previous state was '1', shut down all the LEDs
                                 ball_position <= (others => '0'); 
                             else
-                            -- If previous state was '0', light all LEDs up.
-                                ball_position <= (others => '1'); -- Rozsvítit
+                                ball_position <= (others => '1');
                             end if;
                         end if;
 
-                    -- WINNER DISPLAY
-                        -- Based on whose score is equal to WIN_SCORE
                         if (player_L_score = WIN_SCORE) then
-                            -- 'PLAYER 1'
-                            --                    P     L     A     Y     E     r     _     1
-                            displayed_text <= b"10001_10010_10011_10100_10101_10110_11111_00001";
+                            --                    P     L     A     Y     E     r     _     L
+                            displayed_text <= b"10001_10010_10011_10100_10101_10110_11111_10010";
                         else
-                            -- 'PLAYER 2'
-                            --                    P     L     A     Y     E     r     _     2
-                            displayed_text <= b"10001_10010_10011_10100_10101_10110_11111_00010";
+                            --                    P     L     A     Y     E     r     _     r
+                            displayed_text <= b"10001_10010_10011_10100_10101_10110_11111_10110";
                         end if;
                     
                     when others =>
-                    -- If something unexpected happens, go to 'IDLE' state.
                         game_state <= IDLE;
                 end case;
             
             
-            -- reset 'if'
             end if;
         
-        -- rising_edge 'if'
         end if;
     
-    -- 'game' process
     end process;
 end Behavioral;
--- LED-PingPong/LED-PingPong.srcs/sources_1/new/speed_controller.vhd
+++ LED-PingPong/LED-PingPong.srcs/sources_1/new/speed_controller.vhd
@@ -6,35 +6,35 @@ entity speed_controller is
     Port (
         clk          : in  STD_LOGIC;
         rst          : in  STD_LOGIC;
-        hit_detected : in  STD_LOGIC; -- Puls, když hráč úspěšně odpálí
-        tick_limit   : out integer range 0 to 25_000_000
+        hit_detected : in  STD_LOGIC;
+        tick_limit   : out integer range 0 to 10_000_000
     );
 end speed_controller;
 
 architecture Behavioral of speed_controller is
-    signal hit_count : integer range 0 to 15 := 0;
-    constant ODPALY_PRO_ZRYCHLENI : integer := 2;
+    constant ODPALY_PRO_ZRYCHLENI : integer := 5;
+    constant MAX_COUNT            : integer := (3 * ODPALY_PRO_ZRYCHLENI) - 1;  -- 14
+
+    signal hit_count : integer range 0 to MAX_COUNT := 0;
 begin
-    process(clk)
+
+    -- Hit counter
+    counter_proc : process(clk)
     begin
         if rising_edge(clk) then
             if rst = '1' then
                 hit_count <= 0;
-                tick_limit <= 15_000_000; -- Základní rychlost 150ms
             elsif hit_detected = '1' then
-                if hit_count < (3 * ODPALY_PRO_ZRYCHLENI) - 1 then
+                if hit_count < MAX_COUNT then
                     hit_count <= hit_count + 1;
                 end if;
             end if;
-
-            -- Logika výběru limitu podle počtu odpalů
-            if hit_count < ODPALY_PRO_ZRYCHLENI then
-                tick_limit <= 10_000_000; -- Úroveň 0: 100ms
-            elsif hit_count < (2 * ODPALY_PRO_ZRYCHLENI) then
-                tick_limit <= 7_500_000;  -- Úroveň 1: 75ms
-            else
-                tick_limit <= 5_000_000;  -- Úroveň 2: 50ms
-            end if;
         end if;
     end process;
+
+    -- Speeding up the game depending on the number of successful hits
+    tick_limit <= 10_000_000 when hit_count < ODPALY_PRO_ZRYCHLENI       -- Default: 100 ms
+             else 7_500_000  when hit_count < (2 * ODPALY_PRO_ZRYCHLENI) -- Level 1: 75 ms
+             else 5_000_000;                                             -- Level 2: 50 ms
+
 end Behavioral;
